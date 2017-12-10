239520c8322ad297b348df7275f0a95a0ee2e416
Sven Helmberger <fforw@gmx.de>
2017-12-10 14:38:55 +0100

    Tweak results on playground

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var assign = require("object-assign");
 
+var Math__abs = Math.abs;
+var Math__atan2 = Math.atan2;
+var Math__sqrt = Math.sqrt;
+
+var PI = Math.PI;
+var TAU = PI*2;
+
 /**
  * Higher level helper function to linearize full svg-paths. Supposed to be called by the iterate function
  * of the svgpath library. (NPM "svgpath").
@@ -12,7 +19,7 @@ var assign = require("object-assign");
 function svgPathIterator(segment, index, curX, curY)
 {
     var command = segment[0];
-    var drawLine = this.consumer;
+    var drawLine = al.consumer;
 
     var i, x, y, x2, y2, x3, y3, x4, y4, short = false;
 
@@ -102,19 +109,13 @@ function svgPathIterator(segment, index, curX, curY)
 }
 
 var DEFAULT_OPTS = {
-
-    /**
-     * Maximum error value we allow until we give up and call it a line.
-     */
-    threshold: 0.25,
-    /**
-     * Maximum line length in SVG units. (Default is Infinity). Straight lines above that length
-     * will be chopped down to shorter lines.
-     *
-     * This is useful if you deform the shapes so that formerly straight lines are not straight anymore and would
-     * bend weird if not chopped up into shorter pieces.
-     */
-    maxLine: Infinity
+    approximationScale: 1,
+    curve_distance_epsilon: 1e-30,
+    curveColinearityEpsilon: 1e-30,
+    curveAngleToleranceEpsilon:  0.01,
+    angleTolerance: 0.4,
+    recursionLimit: 32,
+    cuspLimit: 0
 };
 
 function AdaptiveLinearization(consumer, opts)
@@ -131,56 +132,49 @@ function AdaptiveLinearization(consumer, opts)
         throw new Error("maxLine option must be larger than 0");
     }
 
+    var al = this;
 
+    this.consumer = function(x1,y1,x2,y2, data)
+    {
+        al.prevX = x2;
+        al.prevY = y2;
 
-    this.consumer = consumer;
+        consumer(x1,y1,x2,y2, data);
+    };
+
     this.opts = opts;
     this.svgPathIterator  = svgPathIterator.bind(this);
+    this.prevX = false;
+    this.prevY = false;
 
     //console.log("OPTS", opts);
 }
 
-/**
- * Core linearization function linearizes the given bezier curve. Calls the line consumer function registered for
- * the current instance once for every line segment of the linearized curve.
- *
- * @param x1        x-coordinate of the start point
- * @param y1        y-coordinate of the start point
- * @param x2        x-coordinate of the first control point
- * @param y2        y-coordinate of the first control point
- * @param x3        x-coordinate of the second control point
- * @param y3        y-coordinate of the second control point
- * @param x4        x-coordinate of the end point
- * @param y4        y-coordinate of the start point
- * @param data      user data passed on to the comsumer function
- */
-AdaptiveLinearization.prototype.linearize = function(x1, y1, x2, y2, x3, y3, x4, y4, data)
+function distanceTo(x1, y1, x2, y2)
 {
+    var x = x2 - x1;
+    var y = y2 - y1;
 
-    var threshold = this.opts.threshold;
-    var maxLine = this.opts.maxLine;
-    var consumer = this.consumer;
-    
-    if(
-        Math.abs(x1 + x3 - x2 - x2) +
-        Math.abs(y1 + y3 - y2 - y2) +
-        Math.abs(x2 + x4 - x3 - x3) +
-        Math.abs(y2 + y4 - y3 - y3) <= threshold
-    )
-    {
+    return Math__sqrt(x*x+y*y);
+}
 
-        var x = x4 - x1;
-        var y = y4 - y1;
 
-        if (maxLine < Infinity || Math.sqrt(x*x+y*y) <= maxLine)
-        {
-            // Draw and stop
-            //----------------------
-            consumer(x1, y1, x4, y4, data);
-            return;
-        }
-    }
+function linearizeRecursive(al, x1, y1, x2, y2, x3, y3, x4, y4, data, level)
+{
+    var consumer = al.consumer;
+    var opts = al.opts;
 
+
+    var cuspLimit = opts.cuspLimit;
+    var curveColinearityEpsilon = opts.curveColinearityEpsilon;
+    var curveAngleToleranceEpsilon = opts.curveAngleToleranceEpsilon;
+    var angleTolerance = opts.angleTolerance;
+
+
+    var distanceToleranceSquared = 0.5 / opts.approximationScale;
+    distanceToleranceSquared *= distanceToleranceSquared;
+
+    ///////////////////////////////
     // Calculate all the mid-points of the line segments
     //----------------------
     var x12   = (x1 + x2) / 2;
@@ -196,11 +190,225 @@ AdaptiveLinearization.prototype.linearize = function(x1, y1, x2, y2, x3, y3, x4,
     var x1234 = (x123 + x234) / 2;
     var y1234 = (y123 + y234) / 2;
 
+
+    // Try to approximate the full cubic curve by a single straight line
+    //------------------
+    var dx = x4 - x1;
+    var dy = y4 - y1;
+
+    var d2 = Math__abs(((x2 - x4) * dy - (y2 - y4) * dx));
+    var d3 = Math__abs(((x3 - x4) * dy - (y3 - y4) * dx));
+    var da1, da2, k;
+
+    switch(
+    (d2 > curveColinearityEpsilon ? 2 : 0) +
+    (d3 > curveAngleToleranceEpsilon? 1 : 0)
+        )
+    {
+        case 0:
+            // All collinear OR p1==p4
+            //----------------------
+            k = dx*dx + dy*dy;
+            if(k === 0)
+            {
+                d2 = distanceTo(x1, y1, x2, y2);
+                d3 = distanceTo(x4, y4, x3, y3);
+            }
+            else
+            {
+                k   = 1 / k;
+                da1 = x2 - x1;
+                da2 = y2 - y1;
+                d2  = k * (da1*dx + da2*dy);
+                da1 = x3 - x1;
+                da2 = y3 - y1;
+                d3  = k * (da1*dx + da2*dy);
+                if(d2 > 0 && d2 < 1 && d3 > 0 && d3 < 1)
+                {
+                    // Simple collinear case, 1---2---3---4
+                    // We can leave just two endpoints
+                    consumer(x1,y1,x4,y4, data);
+                    return;
+                }
+                if(d2 <= 0) d2 = distanceTo(x2, y2, x1, y1);
+                else if(d2 >= 1) d2 = distanceTo(x2, y2, x4, y4);
+                else             d2 = distanceTo(x2, y2, x1 + d2*dx, y1 + d2*dy);
+
+                if(d3 <= 0) d3 = distanceTo(x3, y3, x1, y1);
+                else if(d3 >= 1) d3 = distanceTo(x3, y3, x4, y4);
+                else             d3 = distanceTo(x3, y3, x1 + d3*dx, y1 + d3*dy);
+            }
+            if(d2 > d3)
+            {
+                if(d2 < distanceToleranceSquared)
+                {
+                    consumer(x1,y1,x2,y2, data);
+                    return;
+                }
+            }
+            else
+            {
+                if(d3 < distanceToleranceSquared)
+                {
+                    consumer(x1,y1, x3, y3, data);
+                    return;
+                }
+            }
+            break;
+
+        case 1:
+            // p1,p2,p4 are collinear, p3 is significant
+            //----------------------
+            if(d3 * d3 <= distanceToleranceSquared * (dx*dx + dy*dy))
+            {
+                if(angleTolerance < curveAngleToleranceEpsilon)
+                {
+                    consumer(x1,y1,x23,y23, data);
+                    return;
+                }
+
+                // Angle Condition
+                //----------------------
+                da1 = Math__abs(Math__atan2(y4 - y3, x4 - x3) - Math__atan2(y3 - y2, x3 - x2));
+                if(da1 >= PI) da1 = TAU - da1;
+
+                if(da1 < angleTolerance)
+                {
+                    consumer(x2,y2,x3,y3, data);
+                    return;
+                }
+
+                if(cuspLimit !== 0.0)
+                {
+                    if(da1 > cuspLimit)
+                    {
+                        consumer(x1,y1,x3,y3, data);
+                        return;
+                    }
+                }
+            }
+            break;
+
+        case 2:
+            // p1,p3,p4 are collinear, p2 is significant
+            //----------------------
+            if(d2 * d2 <= distanceToleranceSquared * (dx*dx + dy*dy))
+            {
+                if(angleTolerance < curveAngleToleranceEpsilon)
+                {
+                    consumer(x1,y1,x23,y23, data);
+                    return;
+                }
+
+                // Angle Condition
+                //----------------------
+                da1 = Math__abs(Math__atan2(y3 - y2, x3 - x2) - Math__atan2(y2 - y1, x2 - x1));
+                if(da1 >= PI) da1 = TAU - da1;
+
+                if(da1 < angleTolerance)
+                {
+                    consumer(x2,y2,x3,y3, data);
+                    return;
+                }
+
+                if(cuspLimit !== 0.0)
+                {
+                    if(da1 > cuspLimit)
+                    {
+                        consumer(x1,y1,x2,y2, data);
+                        return;
+                    }
+                }
+            }
+            break;
+
+        case 3:
+            // Regular case
+            //-----------------
+            if((d2 + d3)*(d2 + d3) <= distanceToleranceSquared * (dx*dx + dy*dy))
+            {
+                // If the curvature doesn't exceed the distance_tolerance value
+                // we tend to finish subdivisions.
+                //----------------------
+                if(angleTolerance < curveAngleToleranceEpsilon)
+                {
+                    consumer(x1,y1,x23,y23, data);
+                    return;
+                }
+
+                // Angle & Cusp Condition
+                //----------------------
+                k   = Math__atan2(y3 - y2, x3 - x2);
+                da1 = Math__abs(k - Math__atan2(y2 - y1, x2 - x1));
+                da2 = Math__abs(Math__atan2(y4 - y3, x4 - x3) - k);
+                if(da1 >= PI) da1 = TAU - da1;
+                if(da2 >= PI) da2 = TAU - da2;
+
+                if(da1 + da2 < angleTolerance)
+                {
+                    consumer(x1,y1,x23,y23, data);
+                    return;
+                }
+
+                if(cuspLimit !== 0.0)
+                {
+                    if(da1 > cuspLimit)
+                    {
+                        consumer(x1,y1,x2,y2, data);
+                        return;
+                    }
+
+                    if(da2 > cuspLimit)
+                    {
+                        consumer(x1,y1,x3,y3, data);
+                        return;
+                    }
+                }
+            }
+            break;
+    }
+
+    var nextLevel = level ? level + 1 : 1;
+
+    if (nextLevel >= opts.recursionLimit)
+    {
+        consumer(x1, y1, x4, y4, data);
+        return;
+    }
+
     // Continue subdivision
     //----------------------
-    this.linearize(x1, y1, x12, y12, x123, y123, x1234, y1234, data);
-    this.linearize(x1234, y1234, x234, y234, x34, y34, x4, y4, data);
+    linearizeRecursive(al, x1, y1, x12, y12, x123, y123, x1234, y1234, data, nextLevel);
+    linearizeRecursive(al, x1234, y1234, x234, y234, x34, y34, x4, y4, data, nextLevel);
+}
+
+/**
+ * Core linearization function linearizes the given bezier curve. Calls the line consumer function registered for
+ * the current instance once for every line segment of the linearized curve.
+ *
+ * @param x1        {number} x-coordinate of the start point
+ * @param y1        {number} y-coordinate of the start point
+ * @param x2        {number} x-coordinate of the first control point
+ * @param y2        {number} y-coordinate of the first control point
+ * @param x3        {number} x-coordinate of the second control point
+ * @param y3        {number} y-coordinate of the second control point
+ * @param x4        {number} x-coordinate of the end point
+ * @param y4        {number} y-coordinate of the start point
+ * @param [data]    {*} user data passed on to the comsumer function
+ */
+AdaptiveLinearization.prototype.linearize = function(x1, y1, x2, y2, x3, y3, x4, y4, data)
+{
+    linearizeRecursive(this, x1, y1, x2, y2, x3, y3, x4, y4, data, 0);
+
+    const prevX = this.prevX;
+    const prevY = this.prevY;
+
+    if (prevX !== x4 || prevY !== y4)
+    {
+        this.consumer(prevX, prevY, x4, y4, data);
+    }
 };
 
+AdaptiveLinearization.DEFAULT_OPTS = DEFAULT_OPTS;
 
 module.exports = AdaptiveLinearization;
